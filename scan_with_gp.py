0bade20dfb2818e4f9dac8617be1ed88f0988da4
Peter Hill <peter.hill@york.ac.uk>
2023-08-23 09:22:00 +0100

    Add command line argument for gs2 bin path

--- scan_with_gp.py
+++ scan_with_gp.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import pyrokinetics as pk
 import xarray as xr
 import subprocess
@@ -45,7 +46,7 @@ def read_growthrate_gpo(base_dir, params, scan):
         return df.omega_average.isel(ri=1, t=-1).squeeze().data
 
 
-def gs2_growth_rate(beta, shat, base):
+def gs2_growth_rate(beta, shat, base, gs2_bin):
     print(f"\n** Running beta={beta}, shat={shat}\n")
 
     params = pk.PyroScan(
@@ -61,63 +62,74 @@ def gs2_growth_rate(beta, shat, base):
     params.write()
 
     run = list(params.pyro_dict.values())[0]
-    run_gs2(run.run_directory, run.file_name)
+    run_gs2(run.run_directory, run.file_name, gs2_bin)
     gamma = read_growthrate(run)
     return -np.abs(gamma)
 
 
-pyro = pk.Pyro(gk_file="fields_local_single_mode.in", gk_code="GS2")
-x = [np.array((0.0, 0.01)), np.array((-1, 5))]
-y = [gs2_growth_rate(*x_, pyro) for x_ in x]
-bounds = [(-1, 0), (0.01, 5)]
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "scan_with_pk.py", description="Uniform scan with pyrokinetics"
+    )
+    parser.add_argument(
+        "--gs2-bin",
+        default="/home/peter/Codes/gyrokinetics/gs2/bin/gs2",
+        help="Path to GS2 executable",
+    )
+    args = parser.parse_args()
 
-scan = pk.PyroScan(
-    pyro,
-    {"beta_prime": [x[0][0]], "shat": [x[0][0]]},
-    value_separator="_",
-    parameter_separator="_",
-    file_name="input.in",
-    base_directory=BASE_DIR,
-)
+    pyro = pk.Pyro(gk_file="fields_local_single_mode.in", gk_code="GS2")
+    x = [np.array((0.0, 0.01)), np.array((-1, 5))]
+    y = [gs2_growth_rate(x[0], x[1], pyro, args.gs2_bin) for x_ in x]
+    bounds = [(-1, 0), (0.01, 5)]
 
-GPO = GpOptimiser(x, y, bounds=bounds, acquisition=UpperConfidenceBound)
+    scan = pk.PyroScan(
+        pyro,
+        {"beta_prime": [x[0][0]], "shat": [x[0][0]]},
+        value_separator="_",
+        parameter_separator="_",
+        file_name="input.in",
+        base_directory=BASE_DIR,
+    )
 
-N = 5
+    GPO = GpOptimiser(x, y, bounds=bounds, acquisition=UpperConfidenceBound)
 
-for _ in tqdm.tqdm(range(N**2 - 2)):
-    new_x = GPO.propose_evaluation()
-    new_y = gs2_growth_rate(new_x[0], new_x[1], pyro)
-    GPO.add_evaluation(new_x, new_y)
+    N = 5
 
-opt_result = np.array([read_growthrate_gpo(BASE_DIR, x_, scan) for x_ in GPO.x])
+    for _ in tqdm.tqdm(range(N**2 - 2)):
+        new_x = GPO.propose_evaluation()
+        new_y = gs2_growth_rate(new_x[0], new_x[1], pyro, args.gs2_bin)
+        GPO.add_evaluation(new_x, new_y)
 
-with open(f"{BASE_DIR}/opt_results.pickle", "wb") as f:
-    pickle.dump(GPO, f)
-    pickle.dump(opt_result, f)
+    opt_result = np.array([read_growthrate_gpo(BASE_DIR, x_, scan) for x_ in GPO.x])
 
-eval_beta = np.array(GPO.x)[:, 0]
-eval_shat = np.array(GPO.x)[:, 1]
+    with open(f"{BASE_DIR}/opt_results.pickle", "wb") as f:
+        pickle.dump(GPO, f)
+        pickle.dump(opt_result, f)
 
-max_growth_rate = np.max(np.abs(opt_result))
+    eval_beta = np.array(GPO.x)[:, 0]
+    eval_shat = np.array(GPO.x)[:, 1]
 
-M = 50
-beta_gp = np.linspace(*bounds[0], M)
-shat_gp = np.linspace(*bounds[1], M)
-beta_ij, shat_ij = np.meshgrid(beta_gp, shat_gp, indexing="ij")
-mean, std = GPO(np.column_stack([beta_ij.flat, shat_ij.flat]))
+    max_growth_rate = np.max(np.abs(opt_result))
 
-fig, ax = plt.subplots()
-cntr = ax.pcolormesh(
-    shat_ij,
-    beta_ij,
-    -mean.reshape((M, M)),
-    cmap="RdBu_r",
-    vmin=-max_growth_rate,
-    vmax=max_growth_rate,
-)
-ax.set_xlabel("shat")
-ax.set_ylabel("beta_prime")
-cbar = fig.colorbar(cntr)
-cbar.set_label("gamma")
-ax.scatter(eval_shat, eval_beta, marker="x", color="k")
-fig.savefig(f"{BASE_DIR}/s_alpha_opt.png")
+    M = 50
+    beta_gp = np.linspace(*bounds[0], M)
+    shat_gp = np.linspace(*bounds[1], M)
+    beta_ij, shat_ij = np.meshgrid(beta_gp, shat_gp, indexing="ij")
+    mean, std = GPO(np.column_stack([beta_ij.flat, shat_ij.flat]))
+
+    fig, ax = plt.subplots()
+    cntr = ax.pcolormesh(
+        shat_ij,
+        beta_ij,
+        -mean.reshape((M, M)),
+        cmap="RdBu_r",
+        vmin=-max_growth_rate,
+        vmax=max_growth_rate,
+    )
+    ax.set_xlabel("shat")
+    ax.set_ylabel("beta_prime")
+    cbar = fig.colorbar(cntr)
+    cbar.set_label("gamma")
+    ax.scatter(eval_shat, eval_beta, marker="x", color="k")
+    fig.savefig(f"{BASE_DIR}/s_alpha_opt.png")
--- scan_with_pk.py
+++ scan_with_pk.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import pyrokinetics as pk
 import xarray as xr
 import subprocess
@@ -9,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def run_gs2(base_dir, filename, gs2_bin="/home/peter/Codes/gyrokinetics/gs2/bin/gs2"):
+def run_gs2(base_dir, filename, gs2_bin):
     old_cwd = os.getcwd()
     os.chdir(base_dir)
     subprocess.run(f"{gs2_bin} {filename}", shell=True, check=True)
@@ -34,36 +35,50 @@ def reshape_results(scan):
     ).reshape(shape)
 
 
-N = 5
-
-pyro = pk.Pyro(gk_file="fields_local_single_mode.in", gk_code="GS2")
-scan = pk.PyroScan(
-    pyro,
-    {"beta_prime": np.linspace(0, -1, N), "shat": np.linspace(0.01, 5, N)},
-    value_separator="_",
-    parameter_separator="_",
-    file_name="input.in",
-    base_directory="salpha_scan_5",
-)
-scan.add_parameter_key("shat", "local_geometry", ["shat"])
-scan.add_parameter_key("beta_prime", "local_geometry", ["beta_prime"])
-scan.write()
-
-for p in scan.pyro_dict.values():
-    # run_gs2(p.run_directory, p.file_name)
-    p.result = read_growthrate(p)
+def plot_gamma(df):
+    pk_B, pk_S = np.meshgrid(df.beta_prime, df.shat, indexing="ij")
+    df.gamma.plot()
+    ax = plt.gca()
+    ax.scatter(pk_S, pk_B, marker="x", color="k")
+    fig = plt.gcf()
+    fig.savefig("s_alpha_pk.png")
 
 
-df = xr.Dataset(
-    {
-        "gamma": (list(scan.parameter_dict.keys()), reshape_results(scan)),
-        **scan.parameter_dict,
-    }
-)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "scan_with_pk.py", description="Uniform scan with pyrokinetics"
+    )
+    parser.add_argument(
+        "--gs2-bin",
+        default="/home/peter/Codes/gyrokinetics/gs2/bin/gs2",
+        help="Path to GS2 executable",
+    )
+    args = parser.parse_args()
 
-pk_B, pk_S = np.meshgrid(df.beta_prime, df.shat, indexing="ij")
-df.gamma.plot()
-ax = plt.gca()
-ax.scatter(pk_S, pk_B, marker="x", color="k")
-fig = plt.gcf()
-fig.savefig("s_alpha_pk.png")
+    N = 5
+
+    pyro = pk.Pyro(gk_file="fields_local_single_mode.in", gk_code="GS2")
+    scan = pk.PyroScan(
+        pyro,
+        {"beta_prime": np.linspace(0, -1, N), "shat": np.linspace(0.01, 5, N)},
+        value_separator="_",
+        parameter_separator="_",
+        file_name="input.in",
+        base_directory="salpha_scan_5",
+    )
+    scan.add_parameter_key("shat", "local_geometry", ["shat"])
+    scan.add_parameter_key("beta_prime", "local_geometry", ["beta_prime"])
+    scan.write()
+
+    for p in scan.pyro_dict.values():
+        run_gs2(p.run_directory, p.file_name, args.gs2_bin)
+        p.result = read_growthrate(p)
+
+    df = xr.Dataset(
+        {
+            "gamma": (list(scan.parameter_dict.keys()), reshape_results(scan)),
+            **scan.parameter_dict,
+        }
+    )
+
+    plot_gamma(df)
